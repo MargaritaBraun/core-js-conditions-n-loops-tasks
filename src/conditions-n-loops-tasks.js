a34ea990bb3692adf26d56902daf808120cfdee6
Margarita Braun <margobraun777@gmail.com>
2025-01-01 21:35:18 +0300

    feat: update shuffleChar

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -532,8 +532,23 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let newStr = str;
+  let left = '';
+  let right = '';
+  for (let i = 0; i < iterations; i += 1) {
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 !== 0) {
+        right += newStr[j];
+      } else {
+        left += newStr[j];
+      }
+    }
+    newStr = left + right;
+    left = '';
+    right = '';
+  }
+  return newStr;
 }
 
 /**
